8bf55b138def5e23aa77e32925d0d1ff9276cec2
Ioannis Tzavaras <warrior.zeus@gmx.de>
2015-08-25 14:37:41 +0200

    Model Users_model.php
    Με το μοντέλο Users_model εκτέλουνται όλες οι ενέργειες που σχετίζονται με την εγγραφή, σύνδεση του χρήστη και τη βάση δεδομένων της εφαρμογής.

---
+++ models/Users_model.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Users_model
+ *
+ * @author zeus
+ */
+class Users_model extends CI_Model {
+    
+    private $result;
+
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function check_email($email) {
+        $sql = "SELECT * FROM users WHERE user_email = '$email'";
+        $this->result = $this->db->query($sql);
+        return $this->result->num_rows();
+    }
+    
+    public function check_username($username) {
+        $sql = "SELECT * FROM users WHERE username = '$username'";
+        $this->result = $this->db->query($sql);
+        return $this->result->num_rows();
+    }
+    
+    public function user_signup($user_email, $username, $user_password) {
+        
+        $data = array(
+            "user_email" => $user_email,
+            "username" => $username,
+            "user_password" => $user_password,
+            "user_type" => "newbee"
+        );
+        
+        $check = $this->db->insert("users", $data);
+        
+        if($check === TRUE) {
+            return array("status" => TRUE, "data" => array("user_email" => $user_email, "username" => $username));
+        } else {
+            return array("status" => FALSE, "msg" => "Η εγγραφή δεν ολοκληρώθηκε!");
+        }
+    }
+    
+    public function user_login($user_email, $user_password) {
+ 
+        $sql = "SELECT * FROM users WHERE user_email = '$user_email' AND user_password = '$user_password'";
+        $this->result = $this->db->query($sql);
+        return $this->result->num_rows();
+    }
+}
